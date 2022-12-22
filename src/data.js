68422a7ef96b8eaa06c18485b2e88c24f21d1ab7
Mass Jack <77391015+massjack1@users.noreply.github.com>
2022-12-22 22:14:28 +0100

    Refractor & create

---
+++ src/data.js
@@ -0,0 +1,22 @@
+export const data = [
+    {
+        id: 1,
+        mainImage: "./img/image-product-1.jpg",
+        thumbnail: "./img/image-product-1-thumbnail.jpg"
+    },
+    {
+        id: 1,
+        mainImage: "./img/image-product-1.jpg",
+        thumbnail: "./img/image-product-1-thumbnail.jpg"
+    },
+    {
+        id: 1,
+        mainImage: "./img/image-product-1.jpg",
+        thumbnail: "./img/image-product-1-thumbnail.jpg"
+    },
+    {
+        id: 1,
+        mainImage: "./img/image-product-1.jpg",
+        thumbnail: "./img/image-product-1-thumbnail.jpg"
+    },
+]
