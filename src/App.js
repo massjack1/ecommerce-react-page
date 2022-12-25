e4ed541d477ce000691a7e44a4d08d4c5ffdc345
Mass Jack <77391015+massjack1@users.noreply.github.com>
2022-12-25 12:13:58 +0100

    Create nav section

--- src/App.js
+++ src/App.js
@@ -4,10 +4,11 @@ import Nav from "./components/Nav";
 import burgermenu from "./components/burgermenu";
 import Carousel from "./components/Carousel";
 import Cart from "./components/Cart";
-import Productphoto from "./components/Productphoto";
+
 
 
 function App() {
+  
   return (
     <div className="div">
       <Nav />
--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,18 +1,23 @@
 import React from "react";
 import logo from "../img/logo.svg";
 import burgermenu from "../components/burgermenu";
+//import Images & icons
+import avatar from "../img/avatar.png";
+import { AiOutlineShoppingCart } from "react-icons/ai";
 //Import Data
 import { data } from "../data";
 
 
 const Nav = () => {
     return (
-    <div>
+        <>
+        <header className="flex items-center justify-between p-8 border-b border-slate-400 max-w-7xl mx-auto">
+    <div className="flex items-center justify-start gap-4">
         <a href="#">
         <img src={logo} alt="Sneaker" />
         </a>
         <nav>
-        <ul>
+        <ul className="font-light text-slate-500 flex items-center justify-center gap-4 mx-auto">
             <li>
                 <a href="#">Collections</a>
             </li>
@@ -29,6 +34,19 @@ const Nav = () => {
         </ul>
         </nav>
     </div>
+    <div>
+        <ul className="flex items-center justify-start gap-4">
+            <li> 
+                <button>
+                    <AiOutlineShoppingCart className="w-20"/>
+                </button>
+                 </li>
+            <li> <img src={avatar} alt="profile photo" className="w-12"/> </li>
+        </ul>
+    </div>
+    </header>
+    </>
+    
     )
 };
 
