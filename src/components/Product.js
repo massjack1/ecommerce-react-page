1e4cd60a1aa3d0320fb7ed67770377f2fdd3f822
Mass Jack <77391015+massjack1@users.noreply.github.com>
2022-12-26 15:29:14 +0100

    Add more styling to the product info

--- src/components/Product.js
+++ src/components/Product.js
@@ -2,18 +2,24 @@ import React from "react";
 
 function Product() {
     return (
-      <div className="container mx-auto py-4">
+      <div className="container mx-auto p-8">
         <div className="flex items-center mb-4">
           <img
             className="w-1/3 rounded-lg shadow-md"
             src=""
             alt="Product"
           />
-          <div className="w-2/3 pl-4">
+          <div className="w-2/3 pl-4 py-8">
           <h1 className="font-bold mb-4 text-orange-400">SNEAKER COMPANY</h1>
-          <h2 className="text-4xl font-bold mb-4">Fall Limited Edition Sneakers</h2>
-            <p className="text-gray-500 font-medium mr-8">These low-profile sneakers are your perfect casual wear companion. Featuring a durable rubber outer sole, they’ll withstand everything the weather can offer.</p>
-            <p className="text-xl font-bold mt-2">$99.99</p>
+          <h2 className="text-6xl font-bold mb-4 ">Fall Limited Edition Sneakers</h2>
+            <div className="m-2">
+            <p className="text-gray-500 font-medium pt-8 text-md leading-5 tracking-wide">These low-profile sneakers are your perfect casual wear companion. Featuring a durable rubber outer sole, they’ll withstand everything the weather can offer.</p>
+            </div>
+            <div className="pt-8 flex flex-row space-x-12">
+            <p className="text-4xl font-bold mt-2">$125.00</p>
+            <p className="text-md mt-2 bg-orange-100 text-orange-500 font-bold py-2 px-2 rounded-md text-medium">50%</p>
+            </div>
+            <p className="pt-6 text-base line-through font-medium text-gray-400">$250.00</p>
             <button className="bg-orange-500 shadow-lg shadow-orange-500/50 hover:bg-orange-400 text-white font-bold py-4 px-12 mt-8 text-lg rounded-md">
               Add to Cart
             </button>
